63210c9603e8ee72036788c4f3973c599d1a0e56
Ekaterina Isakova <kachovv@yandex.ru>
2026-09-13 12:32:33 +0300

    fix: исправлен ErrorHandler

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,51 +1,51 @@
 -- Жанры
-INSERT INTO "Genre" ("GenreId", "Name") VALUES (1, 'Комедия');
-INSERT INTO "Genre" ("GenreId", "Name") VALUES (2, 'Драма');
-INSERT INTO "Genre" ("GenreId", "Name") VALUES (3, 'Мультфильм');
-INSERT INTO "Genre" ("GenreId", "Name") VALUES (4, 'Триллер');
-INSERT INTO "Genre" ("GenreId", "Name") VALUES (5, 'Документальный');
-INSERT INTO "Genre" ("GenreId", "Name") VALUES (6, 'Боевик');
+MERGE INTO "Genre" ("GenreId", "Name") KEY("GenreId") VALUES (1, 'Комедия');
+MERGE INTO "Genre" ("GenreId", "Name") KEY("GenreId") VALUES (2, 'Драма');
+MERGE INTO "Genre" ("GenreId", "Name") KEY("GenreId") VALUES (3, 'Мультфильм');
+MERGE INTO "Genre" ("GenreId", "Name") KEY("GenreId") VALUES (4, 'Триллер');
+MERGE INTO "Genre" ("GenreId", "Name") KEY("GenreId") VALUES (5, 'Документальный');
+MERGE INTO "Genre" ("GenreId", "Name") KEY("GenreId") VALUES (6, 'Боевик');
 
 -- Рейтинги
-INSERT INTO "Rating" ("RatingId", "Name") VALUES (1, 'G');
-INSERT INTO "Rating" ("RatingId", "Name") VALUES (2, 'PG');
-INSERT INTO "Rating" ("RatingId", "Name") VALUES (3, 'PG-13');
-INSERT INTO "Rating" ("RatingId", "Name") VALUES (4, 'R');
-INSERT INTO "Rating" ("RatingId", "Name") VALUES (5, 'NC-17');
+MERGE INTO "Rating" ("RatingId", "Name") KEY("RatingId") VALUES (1, 'G');
+MERGE INTO "Rating" ("RatingId", "Name") KEY("RatingId") VALUES (2, 'PG');
+MERGE INTO "Rating" ("RatingId", "Name") KEY("RatingId") VALUES (3, 'PG-13');
+MERGE INTO "Rating" ("RatingId", "Name") KEY("RatingId") VALUES (4, 'R');
+MERGE INTO "Rating" ("RatingId", "Name") KEY("RatingId") VALUES (5, 'NC-17');
 
 -- Статусы дружбы
-INSERT INTO "FriendStatus" ("StatusId", "Name") VALUES (1, 'Запрошено');
-INSERT INTO "FriendStatus" ("StatusId", "Name") VALUES (2, 'Принято');
+MERGE INTO "FriendStatus" ("StatusId", "Name") KEY("StatusId") VALUES (1, 'Запрошено');
+MERGE INTO "FriendStatus" ("StatusId", "Name") KEY("StatusId") VALUES (2, 'Принято');
 
 -- Пользователи
-INSERT INTO "User" ("UserID", "Email", "Login", "Birthday")
-VALUES (1, 'pepefrog@yandex.ru', 'Pepe', '2006-03-09');
+MERGE INTO "User" ("UserID", "Email", "Login", "Name", "Birthday") KEY("UserID")
+    VALUES (1, 'pepefrog@yandex.ru', 'Pepe', NULL, '2006-03-09');
 
-INSERT INTO "User" ("UserID", "Email", "Login", "Name", "Birthday")
-VALUES (2, 'arbuz13@yandex.ru', 'Watermelon', 'Arbuzitto', '2005-04-17');
+MERGE INTO "User" ("UserID", "Email", "Login", "Name", "Birthday") KEY("UserID")
+    VALUES (2, 'arbuz13@yandex.ru', 'Watermelon', 'Arbuzitto', '2005-04-17');
 
-INSERT INTO "User" ("UserID", "Email", "Login", "Name", "Birthday")
-VALUES (3, 'mikhail_prakticum@yandex.ru', 'mikhail', 'Misha', '2000-01-27');
+MERGE INTO "User" ("UserID", "Email", "Login", "Name", "Birthday") KEY("UserID")
+    VALUES (3, 'mikhail_prakticum@yandex.ru', 'mikhail', 'Misha', '2000-01-27');
 
 -- Дружба
-INSERT INTO "UserFriends" ("UserFriendsId", "UserSenderId", "UserRecipientId", "StatusId")
-VALUES (1, 1, 2, 1);
-INSERT INTO "UserFriends" ("UserFriendsId", "UserSenderId", "UserRecipientId", "StatusId")
-VALUES (2, 1, 3, 2);
-INSERT INTO "UserFriends" ("UserFriendsId", "UserSenderId", "UserRecipientId", "StatusId")
-VALUES (3, 2, 3, 2);
+MERGE INTO "UserFriends" ("UserFriendsId", "UserSenderId", "UserRecipientId", "StatusId") KEY("UserFriendsId")
+    VALUES (1, 1, 2, 1);
+MERGE INTO "UserFriends" ("UserFriendsId", "UserSenderId", "UserRecipientId", "StatusId") KEY("UserFriendsId")
+    VALUES (2, 1, 3, 2);
+MERGE INTO "UserFriends" ("UserFriendsId", "UserSenderId", "UserRecipientId", "StatusId") KEY("UserFriendsId")
+    VALUES (3, 2, 3, 2);
 
 -- Фильмы
-INSERT INTO "Film" ("FilmId", "Name", "Description", "ReleaseDate", "Duration", "RatingId")
-VALUES (1, 'Дедпул', 'Тест', '2016-02-11', 108, 1);
+MERGE INTO "Film" ("FilmId", "Name", "Description", "ReleaseDate", "Duration", "RatingId") KEY("FilmId")
+    VALUES (1, 'Дедпул', 'Тест', '2016-02-11', 108, 1);
 
-INSERT INTO "Film" ("FilmId", "Name", "Description", "ReleaseDate", "Duration", "RatingId")
-VALUES (2, '1+1', 'Тест', '2011-02-26', 112, 3);
+MERGE INTO "Film" ("FilmId", "Name", "Description", "ReleaseDate", "Duration", "RatingId") KEY("FilmId")
+    VALUES (2, '1+1', 'Тест', '2011-02-26', 112, 3);
 
 -- Жанры фильмов
-INSERT INTO "FilmGenre" ("FilmGenreId", "FilmId", "GenreId") VALUES (1, 1, 1);
-INSERT INTO "FilmGenre" ("FilmGenreId", "FilmId", "GenreId") VALUES (2, 1, 2);
-INSERT INTO "FilmGenre" ("FilmGenreId", "FilmId", "GenreId") VALUES (3, 1, 3);
+MERGE INTO "FilmGenre" ("FilmGenreId", "FilmId", "GenreId") KEY("FilmGenreId") VALUES (1, 1, 1);
+MERGE INTO "FilmGenre" ("FilmGenreId", "FilmId", "GenreId") KEY("FilmGenreId") VALUES (2, 1, 2);
+MERGE INTO "FilmGenre" ("FilmGenreId", "FilmId", "GenreId") KEY("FilmGenreId") VALUES (3, 1, 3);
 
 -- Лайки
-INSERT INTO "FilmLikes" ("FilmLikesId", "FilmId", "UserID") VALUES (1, 1, 1);
+MERGE INTO "FilmLikes" ("FilmLikesId", "FilmId", "UserID") KEY("FilmLikesId") VALUES (1, 1, 1);
