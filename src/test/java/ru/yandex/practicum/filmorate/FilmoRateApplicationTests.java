6cc96eacb0ccd14bc587deb1ed2f41721da82597
PepeTheFrogg13 <kolchev.m.a@yandex.ru>
2026-09-17 14:43:43 +0300

    Checkstyle =)

--- src/test/java/ru/yandex/practicum/filmorate/FilmoRateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmoRateApplicationTests.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
-import ru.yandex.practicum.filmorate.dto.EventDto;
 import ru.yandex.practicum.filmorate.dto.ReviewDto;
 import ru.yandex.practicum.filmorate.dto.ReviewNewRequest;
 import ru.yandex.practicum.filmorate.dto.ReviewUpdateRequest;
